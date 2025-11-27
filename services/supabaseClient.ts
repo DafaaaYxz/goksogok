92565d22bc443facc3d1b6ae507d0431ccd863a4
DafaaaYxz <zeuz8686@gmail.com>
2025-11-27 17:14:19 +0700

    Update supabaseClient.ts

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // GANTI DENGAN URL & ANON KEY DARI DASHBOARD SUPABASE ANDA
 // Settings -> API
-const SUPABASE_URL = 'https://lechiyyceexvbwehgelc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlY2hpeXljZWV4dmJ3ZWhnZWxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjExMTIsImV4cCI6MjA3OTczNzExMn0.17D8ybxz43Xxm78s1OX3vb6fjMvWlODhyU_DzIlVsnQ';
+const SUPABASE_URL = 'https://vugvdkkbluvitjpscylu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1Z3Zka2tibHV2aXRqcHNjeWx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNTA0NTEsImV4cCI6MjA3ODcyNjQ1MX0.RY0IyPBR812JDO-rsm7UPaINFYbl65crsBH3CLrW8HM';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
